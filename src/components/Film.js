6a64525677a3e372a287acfb074b60a6a6b2b9f3
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-29 19:13:13 +0000

    film releases homework finished

---
+++ src/components/Film.js
@@ -0,0 +1,11 @@
+const Film = ({name, children}) => {
+    return (
+        <>
+            <li>
+                <a href={children}>{name}</a>
+            </li>
+        </>
+    );
+};
+
+export default Film
---
+++ src/components/FilmList.js
@@ -0,0 +1,19 @@
+import Film from "./Film";
+
+const FilmList = ({films}) => {
+
+    const filmNodes = films.map(film => {
+        return (
+            <Film name = {film.name} key={film.id}>{film.url}</Film>
+        );
+    });
+    return (
+        <>
+            <ul>
+                {filmNodes};
+            </ul>
+        </>
+    );
+};
+
+export default FilmList;
